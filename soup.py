3f150674d705b429f8cff814e79341077914d4b3
nouf yahya <noufyahya1@gmail.com>
2021-05-29 16:01:17 +0400

    new code

--- soup.py
+++ soup.py
@@ -2,27 +2,30 @@
 # using dataframes & pandas & BeautifulSoup
 
 import requests
-from bs4 import BeautifulSoup
-import pandas as pd
+from bs4 import BeautifulSoup as bs
 import csv
+import numpy as np
+import pandas as pd
 
-output = requests.get(
-    'https://www.imdb.com/search/title/?title_type=feature&year=1950-01-01,2020-12-31').text
-soup = BeautifulSoup(output, 'lxml')
-movies = soup.find_all('div', class_='lister-item mode-advanced')
-
-f = csv.writer(open("imbd.csv", "w"))
+f = csv.writer(open("imbd3.csv", "w"))
 # Write column headers as the first line
-f.writerow(["Movie-Name", "Rating", "Director"])
+f.writerow(["Movie-Title", "Director_stars", "Value"])
 
 
 # the code
-for movie in movies:
-    movie_name = movie.find(
-        'h3', class_='lister-item-header').a.text.replace('\n', ' ').strip()
-    url = movie.a['href']
-    movie_stars = movie.find('div', class_='ratings-bar').div.text.strip()
-    _all = movie.find('div', class_='lister-item-content')
-    director = _all.find('p', class_="").text.replace(
-        '\n', '').replace('|    ', ', ').strip()
-    f.writerow([movie_name, movie_stars, director])
+pages = np.arange(1, 1000, 100)
+for page in pages:
+    page = requests.get(
+        "https://www.imdb.com/search/title/?title_type=feature&year=1950-01-01,2020-12-31&start="+str(page)+"&ref_=adv_nxt")
+    soup = bs(page.text, 'html.parser')
+    movies = soup.find_all('div', class_='lister-item mode-advanced')
+
+    for movie in movies:
+        title = movie.find(
+            'h3', class_='lister-item-header').a.text.replace('\n', ' ').strip()
+        director = movie.find('div', class_='lister-item-content').find('p', class_="").text.replace(
+            '\n', '').replace('|    ', ', ').strip()
+
+        f.writerow([title, director])
+
+        #print(f"{movie_title} |{director} |  ")
